47bd8e5ef1188c4dd3c652d6414e444eedc7a280
renaud dor <renaud.dor@gmail.com>
2020-02-10 16:14:40 +0100

    Longest word with rails

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,41 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ('A'..'Z').to_a.sample(1).join
+    end
+    @letters
+  end
+
+  def score
+    @word = params['word'].downcase
+    @grid = params['grid'].downcase
+
+    if !english_word?(@word)
+      @result = 1
+    elsif !word_in_grid?(@word, @grid)
+      @result = 2
+    else
+      @result = 3
+    end
+  end
+
+  def word_in_grid?(word, grid)
+    grid_arr = grid.split('')
+    word.split('').each do |letter|
+      if grid_arr.index(letter)
+        grid_arr.delete_at(grid_arr.index(letter))
+      else
+        return false
+      end
+    end
+  end
+
+  def english_word?(word)
+    # checker que le mot existe (sinon renvoyer 0 et une erreur)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    url_serialized = open(url).read
+    word_dico = JSON.parse(url_serialized)
+    word_dico['found']
+  end
+end
